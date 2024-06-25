8874180939356a33aaec4d616506f570ab708382
masiha <mashiha162@gmail.com>
2024-06-25 16:23:04 +0530

    feat: support different delimiters in Add method

--- src/app/StringCalculator.tsx
+++ src/app/StringCalculator.tsx
@@ -1,7 +1,16 @@
 export class StringCalculator {
   add(numbers: string): number {
+    if (numbers.startsWith("//")) {
+      const delimiterEnd = numbers.indexOf("\n");
+      const delimiter = numbers.substring(2, delimiterEnd);
+      numbers = numbers.substring(delimiterEnd + 1);
+      const numbersArray = numbers
+        .split(new RegExp(`[${delimiter}\n]`))
+        .map(Number);
+      return numbersArray.reduce((acc, num) => acc + num, 0);
+    }
     if (numbers === "") return 0;
-    const numArray = numbers.split(/[\n,]/).map(Number);
-    return numArray.reduce((acc, num) => acc + num, 0);
+    const numbersArray = numbers.split(/[\n,]/).map(Number);
+    return numbersArray.reduce((acc, num) => acc + num, 0);
   }
 }
