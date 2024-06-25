626074ee69f4db205c9629ab2caef08803db284e
masiha <mashiha162@gmail.com>
2024-06-25 16:56:15 +0530

    refactor: restructure add method by extracting helper methods for readability

--- src/app/StringCalculator.tsx
+++ src/app/StringCalculator.tsx
@@ -3,35 +3,49 @@ export class StringCalculator {
 
   add(numbers: string): number {
     this.callCount++;
-    if (numbers.startsWith("//")) {
-      const delimiterEnd = numbers.indexOf("\n");
-      const delimiters = numbers
-        .substring(2, delimiterEnd)
-        .split("][")
-        .map((d) => d.replace(/[\[\]]/g, ""));
-      numbers = numbers.substring(delimiterEnd + 1);
-      const regex = new RegExp(
-        `(${delimiters.map((d) => this.escRegExp(d)).join("|")})|\n`
-      );
-      const numbersArray = numbers
-        .split(regex)
-        .filter(Boolean)
-        .map(Number)
-        .filter((num) => num <= 1000);
-      this.checkForNegatives(numbersArray);
-      return numbersArray.reduce((acc, num) => acc + num, 0);
+    if (this.hasCustomDelimiter(numbers)) {
+      const { delimiters, numbersWithoutDelimiter } =
+        this.extractDelimiters(numbers);
+      return this.calculateSum(numbersWithoutDelimiter, delimiters);
     }
-    if (numbers === "") return 0;
-    const numbersArray = numbers
-      .split(/[\n,]/)
-      .map(Number)
-      .filter((num) => num <= 1000);
-    this.checkForNegatives(numbersArray);
-    return numbersArray.reduce((acc, num) => acc + num, 0);
+    return this.calculateSum(numbers);
   }
 
   getCalledCount(): number {
     return this.callCount;
+  }
+
+  private hasCustomDelimiter(numbers: string): boolean {
+    return numbers.startsWith("//");
+  }
+
+  private extractDelimiters(numbers: string): {
+    delimiters: string[];
+    numbersWithoutDelimiter: string;
+  } {
+    const delimiterEnd = numbers.indexOf("\n");
+    const delimiters = numbers
+      .substring(2, delimiterEnd)
+      .split("][")
+      .map((d) => d.replace(/[\[\]]/g, ""));
+    const numbersWithoutDelimiter = numbers.substring(delimiterEnd + 1);
+    return { delimiters, numbersWithoutDelimiter };
+  }
+
+  private calculateSum(
+    numbers: string,
+    delimiters: string[] = [",", "\n"]
+  ): number {
+    const regex = new RegExp(
+      `(${delimiters.map(this.escRegExp).join("|")})|\n`
+    );
+    const numbersArray = numbers
+      .split(regex)
+      .filter(Boolean)
+      .map(Number)
+      .filter((num) => num <= 1000);
+    this.checkForNegatives(numbersArray);
+    return numbersArray.reduce((acc, num) => acc + num, 0);
   }
 
   private checkForNegatives(numbers: number[]): void {
